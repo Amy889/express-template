f0263e996d27a0ced451c018a8fa60d1d82617e3
“dongxiaoyun” <dongxiaoyun@czb365.com>
2018-07-05 16:26:49 +0800

    fix: 添加body-parser中间件

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express = require('express')
 var app = express()
 var config = require('./config/index')
 var index = require('./router')
-
+var bodyParser = require('body-parser')
+// bodyParser需要放在设置路由之前设置，不然post到数据接受不到参数
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/',index)
 
 var server = app.listen(config.port, function () {
